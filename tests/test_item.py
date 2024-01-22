0907885692d922f638c56be2efb01a8e636fe8ea
Maksim Toguzov <magadan499@mail.ru>
2024-01-22 22:06:18 +0300

    test_apply_discount

--- tests/test_item.py
+++ tests/test_item.py
@@ -3,7 +3,15 @@ import pytest
 from src.item import Item
 
 
-def calculate_total_price():
-    """Проверяем рассчёт общей стоимости"""
+def test_calculate_total_price():
+    """Проверяем расчёт общей стоимости"""
     item1 = Item("Ключ", 100, 20)
     assert item1.calculate_total_price() == 2000
+
+
+def test_apply_discount():
+    """Проверяем стоимость с учетом скидки"""
+    item1 = Item("Ключ", 100, 20)
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 80
