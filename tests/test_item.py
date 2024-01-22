18096e9bb2d8b83c7ed24b644cc8710690e208cf
Maksim Toguzov <magadan499@mail.ru>
2024-01-22 21:43:49 +0300

    test calculation of the total price

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,9 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+def calculate_total_price():
+    """Проверяем рассчёт общей стоимости"""
+    item1 = Item("Ключ", 100, 20)
+    assert item1.calculate_total_price() == 2000
