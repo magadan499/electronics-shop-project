401f03457731e4b2e0777734d6f830cb555e8929
Maksim Toguzov <magadan499@mail.ru>
2024-02-01 21:04:33 +0300

    fix repr

--- src/item.py
+++ src/item.py
@@ -20,7 +20,7 @@ class Item:
         Item.all.append(self)
 
     def __repr__(self):
-        return f'{self.__class__.__name__}({self.__name}, {self.price}, {self.quantity})'
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
 
     def __str__(self):
         return self.__name
