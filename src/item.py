70123fedef78c290ed5cccb73ed07bd18e2e939f
Maksim Toguzov <magadan499@mail.ru>
2024-02-01 20:58:31 +0300

    add repr

--- src/item.py
+++ src/item.py
@@ -19,6 +19,9 @@ class Item:
         self.quantity = quantity
         Item.all.append(self)
 
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.__name}, {self.price}, {self.quantity})'
+
     def calculate_total_price(self) -> float:
         """Рассчитывает общую стоимость конкретного товара в магазине.
         :return: Общая стоимость товара."""
