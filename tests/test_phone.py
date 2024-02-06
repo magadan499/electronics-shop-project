702a1a4314f08afc685255e4ae29e0d97506e3c7
Maksim Toguzov <magadan499@mail.ru>
2024-02-06 20:43:37 +0300

    add test_str

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -5,3 +5,8 @@ def test_str():
     """Проверка наименования"""
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     assert str(phone1) == 'iPhone 14'
+
+def test_repr():
+    """Проверка вывода информации о товаре"""
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
