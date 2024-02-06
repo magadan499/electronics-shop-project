84894add47494799b370814864c6c184fa175c2a
Maksim Toguzov <magadan499@mail.ru>
2024-02-06 21:20:31 +0300

    fix phone

--- src/phone.py
+++ src/phone.py
@@ -9,7 +9,7 @@ class Phone(Item):
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
-        :param sim: Количество сим-карт в телефоне.
+        :param number_of_sim: Количество сим-карт в телефоне.
         """
         super().__init__(name, price, quantity)
         self.__number_of_sim = number_of_sim
@@ -18,7 +18,7 @@ class Phone(Item):
         return self.name
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
 
     @property
     def get_sim(self):
@@ -32,7 +32,3 @@ class Phone(Item):
             raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля')
         else:
             self.__number_of_sim = int(quantity)
-
-
-
-
